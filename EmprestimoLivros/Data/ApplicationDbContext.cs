0c2216aa1fa5e4c86171154b734323ebf875c116
Ruan92929 <ruan92929@hotmail.com>
2023-08-15 08:19:54 -0300

    Finalizacao do CRUD no projeto MVC de emprestimo de livros

---
+++ EmprestimoLivros/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EmprestimoLivros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivros.Data
+{
+    //Nessa classe será realizado a correlação com o Entity Framework.
+   
+    public class ApplicationDbContext :DbContext // Essa classe herda o DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) //A option pega os options do DbContext.
+        { 
+        } 
+
+        //Forma de criar a tabela e como criar uma propriedade
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }//Tabela com estrutura Emprestimos chamada Emprestimos.
+  
+    }
+}
